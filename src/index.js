579e5858521d76d6555cb049f83f6bd0d7d7c813
Wladimyr-R <wlwar@ua.fm>
2021-11-08 20:39:17 +0200

    App переименован

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/component/Home/App';
+// import App from '../src/component/Home/App';
+import App from './component/App/App'
 
 ReactDOM.render(
   <React.StrictMode>
